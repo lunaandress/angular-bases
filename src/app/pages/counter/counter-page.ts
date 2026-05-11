d3fee58e19df56db98003fd9a9f74fac9264c6f1
Andres Luna <miosesfreddy123@gmail.com>
2026-05-11 11:48:31 +0200

    refactor/separate-template-logic

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `
-    <h1>Hola Mundo</h1>
-    <h2>Counter Component Page</h2>
-    <h2>Counter:{{counter}}</h2> <!---esto es una expreciones {{}} donde puede poner valores de mi clase  y mostrarlo en mi paguina--->
-    <button (click)="increaseBy(5)">+1</button><!--Los EVENTOS en angular se ponen en ()-->
+  templateUrl:'./counter-page.html',
+  styles:`
+  button{
+    padding: 5px;
+    margin: 5px 10px;
+    width: 75px;
+  }
   `
 })
+
+
 export class CounterPageComponent {
   counter = 10;
 
-//Funcion
-increaseBy(value : number){
 
+//Funciones
+increaseBy(value : number){
   this.counter+=value;
 }
+
+decrementBY(value : number){
+  this.counter-=value;
+}
+
+restarCounter(){
+  this.counter=0;
+}
+
+
+
+
+
+
+
 }
