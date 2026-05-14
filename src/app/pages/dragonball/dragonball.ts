fdea04426f408d2f7d59ba5c183b0f96ac25242f
Andres Luna <miosesfreddy123@gmail.com>
2026-05-14 11:18:09 +0200

    feature/if-else

--- src/app/pages/dragonball/dragonball.ts
+++ src/app/pages/dragonball/dragonball.ts
@@ -15,11 +15,14 @@ interface Character{
 })
 export class DragonballPageComponent {
 
+  //Señal normal
 characters = signal<Character[]>([
 {id: 1 , name: 'Goku',power:9001},
-{id: 2 , name: 'Vegeta',power:8000},
+{id: 2 , name: 'Vegeta',power:9000},
 {id: 3 , name: 'Picoro',power:3000},
+{id: 4 , name: 'Yamcha',power:500},
 ]);
+
 
 }
 
