b5675aa36d8afa946b1871b3a74b1fd378cfd20c
Andres Luna <miosesfreddy123@gmail.com>
2026-05-14 12:18:05 +0200

    feature/input-event-method-insert character

--- src/app/pages/dragonball/dragonball.ts
+++ src/app/pages/dragonball/dragonball.ts
@@ -4,7 +4,6 @@ interface Character{
   id:number,
   name:string,
   power:number,
-
 }
 
 @Component({
@@ -22,6 +21,35 @@ characters = signal<Character[]>([
 {id: 3 , name: 'Picoro',power:3000},
 {id: 4 , name: 'Yamcha',power:500},
 ]);
+
+
+name=signal('Gohan');
+power=signal(100);
+
+
+addCharacter() {
+  if (!this.name()|| !this.power()|| this.power()<0 ){
+      return;
+  } else {
+      const newCharacter:Character = {
+        id:this.characters.length+1,
+        name:this.name(),
+        power : this.power()
+      };
+
+      //this.characters().push(newCharacter); opcion 1 no recomendada
+      this.characters.update((list)=>[...list,newCharacter]);
+
+      this.resertFile ();
+  }
+
+}
+
+
+  resertFile (){
+    this.name.set('');
+    this.power.set(0);
+  }
 
 
 }
